263a017126123fb4509b93926d2e826d0f20c608
asociationpcl <90176422+asociationpcl@users.noreply.github.com>
2021-11-28 13:39:27 -0500

    Add files via upload

---
+++ count/crear_count.php
@@ -0,0 +1,131 @@
+
+
+<!DOCTYPE html>
+<html style="font-size: 16px;">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <meta name="page_type" content="np-template-header-footer-from-plugin">
+    <title>crear_count</title>
+    <link rel="stylesheet" href="nicepage.css" media="screen">
+<link rel="stylesheet" href="crear_count.css" media="screen">
+    <script class="u-script" type="text/javascript" src="jquery.js" defer=""></script>
+    <script class="u-script" type="text/javascript" src="nicepage.js" defer=""></script>
+    <meta name="generator" content="Nicepage 3.29.1, nicepage.com">
+    
+    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i">
+    
+    <script type="application/ld+json">{
+		"@context": "http://schema.org",
+		"@type": "Organization",
+		"name": ""
+}</script>
+    <meta name="theme-color" content="#478ac9">
+    <meta property="og:title" content="crear_count">
+    <meta property="og:type" content="website">
+  </head>
+  <body class="u-black u-body">
+    <section class="u-align-center u-clearfix u-hidden-lg u-hidden-xl u-video u-video-contain-md u-video-contain-sm u-video-contain-xs u-section-1" id="sec-b8a4">
+      <div class="u-background-video u-expanded u-video-contain-lg u-video-contain-xl u-video-cover-md u-video-cover-sm u-video-cover-xs" style="">
+        <div class="embed-responsive">
+          <video class="embed-responsive-item" data-autoplay="1" loop="" muted="" autoplay="autoplay" playsinline="">
+            <source src="https://pic.pikbest.com/00/91/07/02F888piCdWN.mp4" type="video/mp4">
+            <p>Your browser does not support HTML5 video.</p>
+          </video>
+        </div>
+      </div>
+      <p class="u-align-center-md u-align-center-sm u-align-center-xs u-text u-text-default-lg u-text-default-md u-text-default-xl u-text-palette-3-base u-text-1">SISTEMA DE HACKING PISHING</p><span class="u-align-center u-icon u-icon-circle u-text-palette-1-base u-icon-1"><svg class="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 0 53 53" style=""><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-3a3a"></use></svg><svg class="u-svg-content" viewBox="0 0 53 53" x="0px" y="0px" id="svg-3a3a" style="enable-background:new 0 0 53 53;"><path style="fill:#E7ECED;" d="M18.613,41.552l-7.907,4.313c-0.464,0.253-0.881,0.564-1.269,0.903C14.047,50.655,19.998,53,26.5,53
+	c6.454,0,12.367-2.31,16.964-6.144c-0.424-0.358-0.884-0.68-1.394-0.934l-8.467-4.233c-1.094-0.547-1.785-1.665-1.785-2.888v-3.322
+	c0.238-0.271,0.51-0.619,0.801-1.03c1.154-1.63,2.027-3.423,2.632-5.304c1.086-0.335,1.886-1.338,1.886-2.53v-3.546
+	c0-0.78-0.347-1.477-0.886-1.965v-5.126c0,0,1.053-7.977-9.75-7.977s-9.75,7.977-9.75,7.977v5.126
+	c-0.54,0.488-0.886,1.185-0.886,1.965v3.546c0,0.934,0.491,1.756,1.226,2.231c0.886,3.857,3.206,6.633,3.206,6.633v3.24
+	C20.296,39.899,19.65,40.986,18.613,41.552z"></path><g><path style="fill:#556080;" d="M26.953,0.004C12.32-0.246,0.254,11.414,0.004,26.047C-0.138,34.344,3.56,41.801,9.448,46.76
+		c0.385-0.336,0.798-0.644,1.257-0.894l7.907-4.313c1.037-0.566,1.683-1.653,1.683-2.835v-3.24c0,0-2.321-2.776-3.206-6.633
+		c-0.734-0.475-1.226-1.296-1.226-2.231v-3.546c0-0.78,0.347-1.477,0.886-1.965v-5.126c0,0-1.053-7.977,9.75-7.977
+		s9.75,7.977,9.75,7.977v5.126c0.54,0.488,0.886,1.185,0.886,1.965v3.546c0,1.192-0.8,2.195-1.886,2.53
+		c-0.605,1.881-1.478,3.674-2.632,5.304c-0.291,0.411-0.563,0.759-0.801,1.03V38.8c0,1.223,0.691,2.342,1.785,2.888l8.467,4.233
+		c0.508,0.254,0.967,0.575,1.39,0.932c5.71-4.762,9.399-11.882,9.536-19.9C53.246,12.32,41.587,0.254,26.953,0.004z"></path>
+</g></svg></span>
+      <p class="u-align-center-xs u-text u-text-default u-text-palette-3-base u-text-2">crear cuenta</p>
+      <div class="form u-align-center u-border-3 u-border-custom-color-5 u-form u-radius-12 u-form-1">
+        <form action="../script/ini_count.php" method="POST" class="u-clearfix u-form-custom-backend u-form-spacing-8 u-form-vertical u-inner-form" source="custom" name="form" style="padding: 16px;" redirect="true">
+          <div class="u-form-group u-form-group-1">
+            <label for="text-5489" class="u-label u-text-palette-3-base u-label-1">nombre</label>
+            <input type="text" placeholder="ingresa tu nombre" id="text-5489" name="nombre" class="u-border-2 u-border-black u-gradient u-input u-input-rectangle u-radius-21 u-text-black u-input-1" required="required">
+          </div>
+          <div class="u-form-group u-form-group-2">
+            <label for="text-457e" class="u-label u-text-palette-3-base u-label-2">nacionalidad</label>
+            <input type="text" placeholder="ingresa el nombre de tu pais" id="text-457e" name="pais" class="u-border-2 u-border-black u-gradient u-input u-input-rectangle u-radius-21 u-text-black u-input-2" required="required">
+          </div>
+          <div class="u-form-group u-form-select u-form-group-3">
+            <label for="select-812f" class="u-label u-text-palette-3-base u-label-3">genero</label>
+            <div class="u-form-select-wrapper">
+              <select id="select-812f" name="genero" class="u-border-2 u-border-black u-gradient u-input u-input-rectangle u-radius-21 u-text-black u-input-3" required="required">
+                <option value="Masculino">Masculino</option>
+                <option value="Femenino">Femenino</option>
+              </select>
+              <svg xmlns="http://www.w3.org/2000/svg" width="14" height="12" version="1" class="u-caret"><path fill="currentColor" d="M4 8L0 4h8z"></path></svg>
+            </div>
+          </div>
+          <div class="u-form-group u-form-group-4">
+            <label for="text-edd0" class="u-label u-text-palette-3-base u-label-4">edad</label>
+            <input type="number" placeholder="ingresa tu edad" id="text-edd0" name="edad" class="u-border-2 u-border-black u-gradient u-input u-input-rectangle u-radius-21 u-text-black u-input-4" required="required">
+          </div>
+          <div class="u-form-group">
+            <label for="name-026d" class="u-label u-text-palette-3-base u-label-5">correo electronico</label>
+            <input type="email" placeholder="ingresa tu correo electronico" id="name-026d" name="email" class="u-border-2 u-border-black u-gradient u-input u-input-rectangle u-radius-21 u-text-black u-input-5" required="required">
+          </div>
+          <div class="u-form-group">
+            <label for="email-026d" class="u-label u-text-palette-3-base u-label-6">contraseña</label>
+            <input type="text" placeholder="crear contraseña" id="email-026d" name="clave" class="u-border-2 u-border-black u-gradient u-input u-input-rectangle u-radius-21 u-text-black u-input-6" required="required">
+          </div>
+          <div class="u-form-agree u-form-group u-form-group-7">
+            <input type="checkbox" id="agree-ab0b" name="terminos" class="u-agree-checkbox" required="">
+            <label for="agree-ab0b" class="u-label u-text-palette-3-base u-label-7">&nbsp;Aceptar terminos y condiciones</label>
+          </div>
+          <div class="u-align-left u-form-group u-form-submit">
+            <a href="#" class="u-btn u-btn-round u-btn-submit u-button-style u-hover-palette-1-base u-palette-1-light-2 u-radius-13 u-btn-1">registrarse<br>
+            </a>
+            <input type="submit" value="submit" name="submit" class="u-form-control-hidden">
+          </div>
+          <div class="u-form-send-message u-form-send-success"> Thank you! Your message has been sent. </div>
+          <div class="u-form-send-error u-form-send-message"> Unable to send your message. Please fix errors then try again. </div>
+          <input type="hidden" value="" name="recaptchaResponse">
+        </form>
+      </div>
+      <a href="iniciar_sesion.php" class="u-border-1 u-border-active-palette-2-base u-border-hover-palette-3-base u-border-palette-1-base u-btn u-button-style u-none u-text-active-palette-3-base u-text-hover-palette-3-base u-text-palette-1-base u-btn-2">iniciar sesion</a>
+      <div class="u-shape u-shape-rectangle u-shape-1"></div>
+    </section>
+    <section class="u-clearfix u-hidden-md u-hidden-sm u-hidden-xs u-valign-middle-md u-video u-video-cover u-section-2" id="carousel_f3c8">
+      <div class="u-background-video u-expanded" style="">
+        <div class="embed-responsive embed-responsive-1">
+          <video class="embed-responsive-item" data-autoplay="1" loop="" muted="" autoplay="autoplay" playsinline="">
+            <source src="https://pic.pikbest.com/00/91/07/02F888piCdWN.mp4" type="video/mp4">
+            <p>Your browser does not support HTML5 video.</p>
+          </video>
+        </div>
+      </div>
+      <div class="u-clearfix u-sheet u-sheet-1">
+        <div class="u-clearfix u-expanded-width-sm u-expanded-width-xs u-layout-wrap u-layout-wrap-1">
+          <div class="u-layout">
+            <div class="u-layout-row">
+              <div class="u-align-center u-container-style u-layout-cell u-right-cell u-shape-rectangle u-size-27-lg u-size-27-xl u-size-29-md u-size-29-sm u-size-29-xs u-layout-cell-1">
+                <div class="u-container-layout u-container-layout-1">
+                  <img class="u-expanded-width u-image u-image-contain u-image-default u-image-1" src="images/08bc96b9-1ab5-5e91-81df-0f995f07ac021.png" alt="" data-image-width="1153" data-image-height="617">
+                  <p class="u-text u-text-palette-3-base u-text-1">LO SENTIMOS, esta pagina solo esta disponible para dispositivos moviles</p>
+                </div>
+              </div>
+              <div class="u-container-style u-layout-cell u-left-cell u-size-31-md u-size-31-sm u-size-31-xs u-size-33-lg u-size-33-xl u-layout-cell-2">
+                <div class="u-container-layout u-valign-middle u-container-layout-2">
+                  <img class="u-expanded-width u-image u-image-contain u-image-2" src="images/-min.png" data-image-width="1126" data-image-height="700">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </body>
+</html>
